25e57ddb2141e80ab2cb6c1a29e581b059391eb3
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-23 17:46:03 +0900

    2025.05.23 jdbc 예외처리 내용 커밋

---
+++ jdbc04_Employee/src/com/jdbc/exception/DuplicateNumException.java
@@ -0,0 +1,10 @@
+package com.jdbc.exception;
+
+public class DuplicateNumException extends Exception {
+	public DuplicateNumException() {
+		this("This is DuplicateNumException...");
+	}
+	public DuplicateNumException(String message) {
+		super(message);
+	}
+}
